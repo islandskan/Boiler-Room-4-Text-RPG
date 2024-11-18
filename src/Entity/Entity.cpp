c7020ba6f64aa5e15f6bb0d9ac253ea07bd39d77
LindeborgLove <lindeborglove@gmail.com>
2024-11-18 16:18:41 +0100

    Added entity class implementations

---
+++ src/Entity/Entity.cpp
@@ -0,0 +1,23 @@
+#include "Entity.h"
+
+bool Entity::isDead()
+{
+    return this->m_health < 0;
+}
+
+void Entity::heal(int hp)
+{
+    this->m_health += hp;
+    if (m_health > m_maxHealth)
+        m_health = m_maxHealth;
+}
+
+int Entity::getHealth()
+{
+    return m_health;
+}
+
+int Entity::getDamage()
+{
+    return m_damage;
+}
